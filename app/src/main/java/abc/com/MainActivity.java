50aedb8a0f0218e60092339c3b05f03083ae12c9
nayyar <nayyar.syed@gmail.com>
2019-09-11 18:06:25 +1200

    Initial commit

---
+++ app/src/main/java/abc/com/MainActivity.java
@@ -0,0 +1,24 @@
+package abc.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+      setContentView( R.layout.activity_main );
+
+    }
+}
